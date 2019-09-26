7c0ee5192003b43590976f40670e4cf1141266cc
till aust <till.aust@student.uni-luebeck.de>
2019-09-26 11:38:12 +0200

    controlling my little friend

--- main.m
+++ main.m
@@ -9,14 +9,18 @@ portNumber = 20000; % FOR KUKA: 19999
 %% FEEL FREE TO TOUCH
 % parameter abhaengig vom Roboter
 numberOfJoints = 4; %FOR KUKA 7
-jointName = 'MTB_axis%i'; % FOR KUKA: 'LBR_iiwa_7_R800_joint%i' 'MTB_axis%i'
+jointName = 'uarm_motor%i'; % FOR KUKA: 'LBR_iiwa_7_R800_joint%i' 'MTB_axis%i'
 %verbindet zum ROboterarm
 VREPconnect(portNumber, jointName, 1);
 
 % hier kann man die gelenkparameter einstellen
-q = [100 100 0 0]; %ones(numberOfJoints,1)*20;
-%fahr Parameter an
-VREPtransfer(q);
-        
+while true
+    %q = zeros(4,1)%rand(4,1) * 60
+    % erster wert drehen in xy ebene
+    % zweiter wert 
+    q = input('bitte Gelenkwinkel angeben \n >_ ')
+    %fahr Parameter an
+    VREPtransfer(q);
+end
 % trennt die Verbindung
 VREPdisconnect;
