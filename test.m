ff0e2234820ebd96887503a26caf1faffb4df027
till aust <till.aust@student.uni-luebeck.de>
2019-10-10 14:18:27 +0200

    check if fkine is correct

--- test.m
+++ test.m
@@ -15,11 +15,16 @@ jointName = 'joint%i'; % FOR KUKA: 'LBR_iiwa_7_R800_joint%i' 'MTB_axis%i'
 %verbindet zum ROboterarm
 VREPconnect(portNumber, jointName, 1);
 VREPtransfer([0 0 0 0])
-
-while 1
-    q = input('bitte Gelenkwinkel angeben \n >_ ');
+% gelenkwinkel konfigs
+kfg = [pi/2 pi/2 pi/2 pi/2];
+i = 1;
+while i < 2
+    %q = input('bitte Gelenkwinkel angeben \n >_ ');
+    q = kfg(i,:);
     %fahr Parameter an
     VREPtransfer(q);
+    i = i+1;
+    pause();
 end
 % trennt die Verbindung
 VREPdisconnect;
@@ -27,10 +32,12 @@ VREPdisconnect;
 %% forward kinematics -> fragen nach code
 % parameter
 % a alpha d theta
-DHtabelle = [0.0000 -90.0 0.3450 90.0;
+DH = [0.0000 -90.0 0.3450 90.0;
             0.0000 90.0 -0.2500 -90.0;
             0.0 0.2500 0.0000 -90.0
             0.3271 0.0 0.0 1.3];
-        
+
+T = fkine_dh(4, kfg(1,:), DH(:,3)', DH(:,1)', DH(:,2)', DH(:,4)');
+pos = T(1:3,4,5)        
         
 
